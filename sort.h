6cff476f76f5acff6d512b9757dcfeae488b12ea
Abdessattar <ghabi.1602@gmail.com>
2024-01-19 14:32:19 +0000

    Create the header file

---
+++ sort.h
@@ -0,0 +1,26 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* libraries declaration section */
+
+
+/* structures declaration section */
+
+/**
+ * struct listint_s - doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* prototypes declaration section */
+void print_array(const int *, size_t);
+void print_list(const listint_t *);
+void bubble_sort(int *array, size_t size);
