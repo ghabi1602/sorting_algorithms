04949cef6e0d50342c34851c53b63e74ef6d10c4
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-20 12:59:06 -0800

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - swap the nodes at a point
+ * @node1: the first node
+ * @node2: the second node
+ */
+
+void swap_nodes(listint_t *node1, listint_t *node2)
+{
+	if(node1->prev)
+		node1->prev->next = node2;
+	if(node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+}
+
+
+/**
+ * insertion_sort - swap nodes of doubly linked list
+ * till the array is sorted
+ * @list: pointer to doubly linked list
+ * 
+ * Return: None
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *next_node, *next_2_node;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	next_node = (*list)->next;
+	while(next_node != NULL)
+	{
+		next_2_node = next_node;
+		next_node = next_node->next;
+		
+		while(next_2_node && next_2_node->prev)
+		{
+			if(next_2_node->prev->n > next_2_node->n)
+			{
+				swap_nodes(next_2_node->prev, next_2_node);
+				if(next_2_node->prev == NULL)
+					*list = next_2_node;
+				print_list((const listint_t *)*list);
+			}
+			else
+				next_2_node = next_2_node->prev;
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -3,6 +3,8 @@
 
 /* libraries declaration section */
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* structures declaration section */
 
@@ -24,6 +26,10 @@ typedef struct listint_s
 void print_array(const int *, size_t);
 void print_list(const listint_t *);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
+
+/* extra function */
+void swap_nodes(listint_t *node1, listint_t *node2);
 
 #endif
