48700bf860a7df0968cd409ef8d9ee27806ad199
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-21 12:54:56 -0800

    selection sort

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* structures declaration section */
 
@@ -27,7 +28,7 @@ void print_array(const int *, size_t);
 void print_list(const listint_t *);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
 
 /* extra function */
 void swap_nodes(listint_t *node1, listint_t *node2);
